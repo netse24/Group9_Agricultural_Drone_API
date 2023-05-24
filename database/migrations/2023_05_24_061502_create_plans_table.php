0ce98a0cb3485ac64203021af489cd537c469e5b
PHANNITPHUOY <phannitphuoy1102@gmail.com>
2023-05-24 13:54:52 +0700

    Create tables, models and controllers.

---
+++ database/migrations/2023_05_24_061502_create_plans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('type_job');
+            $table->string('date_time');
+            $table->string('area');
+            $table->unsignedBigInteger('farmer_id');
+            $table->foreign('farmer_id')->references('id')->on('farmers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plans');
+    }
+};
