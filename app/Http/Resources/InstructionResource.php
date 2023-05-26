9453eedfd1f6f382e7ffa2a8b78d9097697af83f
NetSe24 <netse0962@gmail.com>
2023-05-26 16:55:32 +0700

    Request plan with instructions

---
+++ app/Http/Resources/InstructionResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class InstructionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'instruction' => $this->instruction,
+            'instruct to drone' => new DroneResource($this->drone)
+        ];
+    }
+}
---
+++ app/Http/Resources/ShowPlanResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShowPlanResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->type_job,
+            'date_time' => $this->date_time,
+            'area' => $this->area,
+            'instructions' => InstructionResource::collection($this->instructions)
+
+        ];
+    }
+}
